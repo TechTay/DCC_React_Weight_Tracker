e205431bc6c34482e334b649d62043b7887ad063
DeVante Parker <Devante.parker15@gmail.com>
2023-02-06 21:19:43 -0600

    Completed Entry Chart Tracker Component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React, { useState } from 'react';
 import DisplayEntries from './Components/DisplayEntries/DisplayEntries';
 import AddEntryForm from './Components/DisplayEntries/AddEntry/AddEntryForm';
+import EntriesChartTracker from './Components/EntriesChartTracker/EntriesChartTracker';
 
 function App() {
 
 const[entries, setEntries] = useState([{weight: 175, Date: '1-23-2023'}])
 
 function addNewEntry(entry){
+
   let tempEntries = [...entries, entry];
+
   setEntries(tempEntries);
 }
 
@@ -15,6 +18,7 @@ function addNewEntry(entry){
     <div>
      <DisplayEntries parentEntries={entries} />
      <AddEntryForm addNewEntryProperty={addNewEntry}/>
+     <EntriesChartTracker parentEntries={entries}/>
     </div>
   );
 }
